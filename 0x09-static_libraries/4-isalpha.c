76f62ce5697600885066e5e38bf80067a85cffc6
Sunnywayne7 <akandesunday70@gmail.com>
2022-10-05 21:58:45 +0000

    Static libraries

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+*_isalpha-function that checks for alphabetic character
+*@c:parameter member c
+*Return: returns 1 or 0 is success
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
