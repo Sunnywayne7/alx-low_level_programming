9b4bcb58770ff0341bca4249d4964a65e96509d1
Sunnywayne7 <akandesunday70@gmail.com>
2022-10-20 22:05:39 +0000

    singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return.
  */
 
-void _attribute_ ((constructor)) bmain()
+void _attribute_ ((constructor)) main()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
